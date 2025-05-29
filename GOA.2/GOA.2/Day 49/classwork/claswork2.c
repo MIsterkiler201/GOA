5c8455a9a7d4bf48f7df98a5d793707dbf84682e
MIsterkiler201 <irakligogiava7@gmail.com>
2025-05-29 22:39:39 +0400

    Add files via upload

---
+++ GOA.2/GOA.2/Day 49/classwork/claswork2.c
@@ -0,0 +1,37 @@
+center{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+}
+
+body{
+    background: #231f20;
+    font-family: Arial, Helvetica, sans-serif;
+
+}
+
+.circle{
+    background: red;
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    margin-right: 20px;
+}
+
+h1{
+    font-weight: 200;
+}
+[10:52 PM]
+shecvale ogond
+[10:52 PM]
+gesmis
+[10:52 PM]
+saxelebi
+[10:52 PM]
+tore mixdeba
+
+
+Message @774681
